7a0b70679f544441c54d4a7e9573ca2da903082f
arthurdobrea <adamaa14@gmail.com>
2017-07-05 13:30:52 +0300

    Update User.java

--- src/main/java/net/arthur/springsecurityapp/model/User.java
+++ src/main/java/net/arthur/springsecurityapp/model/User.java
@@ -3,12 +3,7 @@ package net.arthur.springsecurityapp.model;
 import javax.persistence.*;
 import java.util.Set;
 
-/**
- * Simple JavaBean domain object that represents a User.
- *
- * @author Eugene Suleimanov
- * @version 1.0
- */
+
 
 @Entity
 @Table(name = "users")
